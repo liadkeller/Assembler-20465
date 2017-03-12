c91562a8c8534f69cd382f18224b33fc0996c74d
liadkeller <liadkeller@gmail.com>
2017-03-12 19:02:15 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+
 #define assembly_line_max 100
 #define IC_start 100
 enum BOOL {FALSE , TRUE}
+
+int isCode(char *cmd); // from analysis.c
+
+void firstLoop(FILE *);
+void secondLoop();
