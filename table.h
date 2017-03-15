936ec9916319a7ceafc8ad763964c0cc10a74011
liadkeller <liadkeller@gmail.com>
2017-03-15 12:48:45 +0200

    Update table.h

--- table.h
+++ table.h
@@ -1,9 +1,27 @@
+/*
+נסביר את השימוש בקיצורים הבאים
+
+cmd - command - פקודה
+מצד אחד משתמשים כדי לתאר משפטי פקודה/משפטי פעולה המורכבים ממהוראה ואופרנדים, לדוגמה
+mov, inc, cmp, ...
+מצד שני משתמשים כדי לתאר פקודה כלשהי, באופן כללי, הן פקודות שהן משפטי פעולה והן פקודות שהן משפטי הוראה
+Code Command, Data Command, ...
+
+data - נתונים
+מצד אחד משתמשים כדי לתאר משפטי הוראה של מספרים שלמים המתחילות ב
+.data
+מצד שני משתמשים כדי לתאר באופן כללי משפטי הוראה המכילים נתונים, גם מספרים שלמים וגם מחרוזות, כלומר
+.data and .string
+*/
+
 enum encode {NUMBER, ADDRESS, NUM2, ONE_REGISTER, TWO_REGISTER, MAIN_COMMAND}
 enum encodeType {A, R, E}
 
 void addCmdToList(struct cmd *c, struct list *t);
-void addCmd(char *cmd, int address);
-struct cmd *makeNext(int encode, int address);
+int addCmd(char *cmd, int address);
+void addDataToList(struct cmd *c, struct list *t);
+int addData(char *cmd, int address);
+int addStr(char *cmd, int address);
 
 struct cmd {
 	int encode;
@@ -32,10 +50,11 @@ struct cmd {
 
 struct data {
 	int isFirst;
-	// int dataOrString (enum DtSt {DATA STR})
 	int wordsNum;
 	int address;
 	int content;
+	int isSymbol;
+	char *symbol;
 	
 	struct data *next;
 }
