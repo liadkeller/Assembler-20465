7d9a3d2c511a2cabd316eda3e2b095beba8e7870
liadkeller <liadkeller@gmail.com>
2017-03-15 13:45:27 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,4 +1,20 @@
 #include <string.h>
+
+#define data_length 5 // .data
+#define string_length 7 // .string
+
 #define op_name_size 3
 #define op_num 16
+
 enum BOOL {FALSE , TRUE}
+
+int isSymbol(char *cmd); // CMD = code OR data
+char *getSymbol(char *cmd); // CMD = code OR data
+int getCmdStart(char *cmd); // CMD = code OR data
+int isCode(char *cmd);
+int isData(char *cmd); // CMD = code OR data
+int isStr(char *cmd); // CMD = code OR data
+int getOpcode(char *op);
+int getGroup(char *op);
+char *getFirstOperand(char *cmd);
+char *getSecondOperand(char *cmd);
