a015d57db2e5a3bcb8939533486d6879032e5736
liadkeller <liadkeller@gmail.com>
2017-03-16 12:25:36 +0200

    Update symbol.c

--- symbol.c
+++ symbol.c
@@ -1,14 +1,16 @@
 struct symbol {
         char *label;
+        int type;
         int address;
         struct symbol *next;
 } *symbolTable = NULL;
 
-addSymbol(char *label, int address)
+addSymbol(char *label, int type, int address)
 {
         struct symbol *new;
         new = (struct symbol *) malloc (sizeof (struct symbol)); // malloc - requires to free the allocation
         new->label = label;
+        new->type = type;
         new->address = address;
         new->next = NULL;
         
