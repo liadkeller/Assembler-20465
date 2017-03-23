9164f78f0d4670e6c2f3999ec5bc9be7035701b1
anuk8 <anuk8@walla.com>
2017-03-23 15:52:57 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,20 +1,21 @@
 #include <string.h>
+#include <stdlib.h>
 
-#define data_length 5 // .data
-#define string_length 7 // .string
-#define extern_length 7 // .extern
+#define data_length 5 /* .data*/
+#define string_length 7 /* .string*/
+#define extern_length 7 /* .extern*/
 
 #define op_name_size 3
 #define op_num 16
 
-enum BOOL {FALSE , TRUE}
+enum BOOL {FALSE , TRUE};
 
-int isSymbol(char *cmd); // CMD = code OR data
-char *getSymbol(char *cmd); // CMD = code OR data
-int getCmdStart(char *cmd); // CMD = code OR data
+int isSymbol(char *cmd); /* CMD = code OR data*/
+char *getSymbol(char *cmd); /* CMD = code OR data*/
+int getCmdStart(char *cmd); /* CMD = code OR data*/
 int isCode(char *cmd);
-int isData(char *cmd); // CMD = code OR data
-int isStr(char *cmd); // CMD = code OR data
+int isData(char *cmd); /* CMD = code OR data*/
+int isStr(char *cmd); /* CMD = code OR data*/
 int isExt(char *cmd);
 int getOpcode(char *op);
 int getGroup(char *op);
