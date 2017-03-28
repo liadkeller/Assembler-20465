6e45166128ac12cc4de8742117c778e6c069f037
anuk8 <anuk8@walla.com>
2017-03-28 20:44:17 +0300

    Update main.h

--- main.h
+++ main.h
@@ -9,8 +9,11 @@ int isBlankOrComment(char *cmd);
 int isCode(char *cmd);
 int isData(char *cmd);
 int isStr(char *cmd);
+int isExt(char *cmd);
+int isEnt(char *cmd);
 
 int addCmd(char *cmd, int address);
+int addExt(char *cmd);
 int addData(char *cmd, int address);
 int addStr(char *cmd, int address);
 
