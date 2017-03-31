d905b24d156667af29b1e255f64d4225eab1de56
liadkeller <liadkeller@gmail.com>
2017-04-01 01:49:22 +0300

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,4 +1,5 @@
 int encode(struct cmd *code, int encode);
+
 int intToBinary(int num, int size) /* returns the binary presentation of the first "size" digits of num */
 {
         int arr[size];
@@ -68,4 +69,66 @@ int pow(int a, int b)
         int i, res;
         for(i = 0, res = 1; i < b; i++, res *= a);
         return res;
+}
+
+/* ==================== 
+      HEXADECIMAL
+===================== */
+
+char *codeToHexa(int bin) /* gets 15-digits binary num and returns hexa string */
+{
+        int i;
+        char hex[4];
+        char ans[5]; /* !!! requires allocation */
+        
+        int t;
+        
+        for(i = 0; i < 4; i++)
+        {
+                t = (i / pow(10, 4*i)) % pow(10, 4);
+                hex[i] = singleHex(t);
+        }
+        
+        for(i = 0; i < 4; i++)
+                ans[i] = hex[4-i];
+        ans[i] = '\0';
+        
+        return ans;
+}
+
+char singleHexa(int bin) /* gets 4-digits binary num and returns hexa char */
+{
+        int num = 0;
+        
+        for(i = 0; i < 4; i++)
+        {
+                if(bin % 10)
+                        num += pow(2, i);
+        }   
+        
+        if(num >= 0 && num <= 9)
+                return '0' + num;
+        
+        if(num >= 10 && num <= 15)
+                return 'A' + num - 10;
+}
+
+char *addressToHexa(int num) /* gets decimal num smaller ehan 256 and returns hexa string */
+{
+        char hex[3]; /* !!! requires allocation */
+        
+        hex[2] = '\0';
+        hex[1] = intToHexa(num % 16);
+        hex[0] = intToHexa(num / 16);
+        
+        return hex;
+}
+
+char intToHexa(int num)
+{
+        if(num >= 0 && num <= 9)
+                return '0' + num;
+        
+        if(num >= 10 && num <= 15)
+                return 'A' + num - 10;
 }
