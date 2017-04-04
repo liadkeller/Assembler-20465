077fb21bfd655d4079d8700eb920e40f5a38637c
liadkeller <liadkeller@gmail.com>
2017-04-04 15:09:03 +0300

    Create binary.h

---
+++ binary.h
@@ -0,0 +1,18 @@
+
+void secondLoop()
+void buildOperand(struct cmd *c, char *operand)
+
+char *encode(struct cmd *code, int encode)
+char *intToBinary(int num, int size)
+void addBin(char *bin, char *num, int start, int size)
+char *binaryToHexa(char *bin)
+int getHexa(int num)
+
+void addBinary(int address, char *bin)
+
+struct binarycode
+{
+        int address;
+        char binary[BINARY_WORD];
+        struct binarycode *next;
+}
