9fb9f9d38e9f298ccc13018bd8b10f23e7ca807d
liadkeller <liadkeller@gmail.com>
2017-04-06 16:56:14 +0300

    Update binary.h

--- binary.h
+++ binary.h
@@ -1,14 +1,14 @@
 
-void secondLoop()
-void buildOperand(struct cmd *c, char *operand)
+void secondLoop();
+void buildOperand(struct cmd *c, char *operand);
 
-char *encode(struct cmd *code, int encode)
-char *intToBinary(int num, int size)
-void addBin(char *bin, char *num, int start, int size)
-char *binaryToHexa(char *bin)
-int getHexa(int num)
+char *encode(struct cmd *code, int encode);
+char *intToBinary(int num, int size);
+void addBin(char *bin, char *num, int start, int size);
+char *binaryToHexa(char *bin);
+int getHexa(int num);
 
-void addBinary(int address, char *bin)
+void addBinary(int address, char *bin);
 
 struct binarycode
 {
