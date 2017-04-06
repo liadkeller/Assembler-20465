a2dc957ffc62c151bb2fa3f5c6fcdb36afa40962
liadkeller <liadkeller@gmail.com>
2017-04-07 00:51:12 +0300

    Create files.c

---
+++ files.c
@@ -0,0 +1,55 @@
+void createObject(FILE *f)
+{
+        fprintf(f, "%X   %X\n, IC - IC_start, DC);
+        
+        struct binarycode *cur = binaryTable;
+        while(cur && cur->next)
+        {
+                fprintf("%X   %s", cur->address, cur->binary);
+                cur = cur->next;
+        }
+}
+
+void createEntry(FILE *f)
+{
+
+}
+
+void createExtern(FILE *f)
+{
+        struct cmd cur = table.cmdHead;
+        while(cur && cur->next)
+        {
+                if(cur->group > 0)
+                {
+                        if(cur->firstAddressing == ADDRESS)
+                        {
+                                struct symbol symbolCur = symbolTable;
+                                while(symbolCur && symbolCur->next)
+                                {
+                                        if(strcmp(symbolCur->name, cur->firstOperand) == 0)
+                                                if(symbolCur->type == EXT)
+                                                        fprintf("%s   %X", symbolCur->name, cur->address);
+                                        symbolCur = symbolCur->next;
+                                }
+                        }
+                }
+        
+                if(cur->group > 1)
+                {
+                        if(cur->secndAddressing == ADDRESS)
+                        {
+                                struct symbol symbolCur = symbolTable;
+                                while(symbolCur && symbolCur->next)
+                                {
+                                        if(strcmp(symbolCur->name, cur->secndOperand) == 0)
+                                                if(symbolCur->type == EXT)
+                                                        fprintf("%s   %X", symbolCur->name, cur->address);
+                                        symbolCur = symbolCur->next;
+                                }
+                        }
+                }
+                
+                cur = cur->next;
+        }
+}
