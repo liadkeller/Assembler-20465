5bb65dd34dd0e73b8fe230506a2894b082ea33fb
anuk8 <anuk8@walla.com>
2017-04-07 03:42:25 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,24 +1,10 @@
 #include "utils.h"
+#include "binary.h"/*+table.h */
 
-int isBlankOrComment(char *cmd);
-int isCode(char *cmd);
-int isData(char *cmd);
-int isStr(char *cmd);
-int isExt(char *cmd);
-int isEnt(char *cmd);
-
-int addCmd(char *cmd, int address);
-int addExt(char *cmd);
-int addData(char *cmd, int address);
-int addStr(char *cmd, int address);
-
-void fixAddresses(int add);
-void buildSymbolTable(void);
-
-void firstLoop(FILE *);
+void firstLoop(FILE *f,int *ICp,int *DCp);
 void secondLoop();
 
-void createObject(FILE *f);
+void createObject(FILE *f,int IC, int DC);
 void createEntry(FILE *f);
 void createExtern(FILE *f);
 
